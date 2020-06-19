e32ee37b8890e31e3fa2f41644733c265dc8ac96
Michael Blume <blume.mike@gmail.com>
2020-06-18 21:26:17 -0700

    Upgrade Clojure

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.3.1"]])
